80b64cc16affe68e57b81ae125b06e4424b6ff67
yuweiZhan <195812208@qq.com>
2020-09-06 15:23:22 +0800

    :sparkles: 以分区为单位，对RDD中的元素进行映射，并且带分区编号

--- src/main/scala/com/example/spark/day03/Spark02MapPartitionsWithIndex.scala
+++ src/main/scala/com/example/spark/day03/Spark02MapPartitionsWithIndex.scala
@@ -2,7 +2,10 @@ package com.example.spark.day03
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark01MapPartitions {
+/**
+ * 以分区为单位，对RDD中的元素进行映射，并且带分区编号
+ */
+object Spark02MapPartitionsWithIndex {
 
   def main(args: Array[String]): Unit = {
     // 创建SparkConf并设置App名称
@@ -13,14 +16,10 @@ object Spark01MapPartitions {
 
     val rdd = sc.makeRDD(List(1, 2, 3, 4),numSlices = 2)
 
-    // 以分区为单位，对RDD中的元素进行映射
-    // 一般使用与批处理的操作，比如：将RDD中的元素插入到数据库中，需要数据库连接
-    val newRdd = rdd.mapPartitions(datas => {
-      datas.map(_ * 2)
+    val newRdd = rdd.mapPartitionsWithIndex((index, datas) => {
+      datas.map((index, _))
     })
-
     newRdd.collect().foreach(println)
-
     // 关闭连接
     sc.stop()
   }
