87111270f4fdb9cd18571f691608dd3ec0591def
yuweiZhan <195812208@qq.com>
2020-09-26 11:47:21 +0800

    :sparkles: spark-sql2

--- src/main/scala/com/example/spark/sql/Intro.scala
+++ src/main/scala/com/example/spark/sql/Intro.scala
@@ -30,4 +30,22 @@ class Intro {
 
     resultDs.show()
   }
+
+  @Test
+  def dfIntro(): Unit ={
+    val spark = new SparkSession.Builder()
+      .appName("ds intro")
+      .master("local[6]")
+      .getOrCreate()
+
+    val sourceRDD = spark.sparkContext.parallelize(Seq(Person("zhangsan", 10), Person("lisi", 15)))
+    import spark.implicits._
+    val df = sourceRDD.toDF()
+
+    df.createOrReplaceTempView("person")
+
+    val resultDf = spark.sql("select name from person where age >10 and age <20")
+
+    resultDf.show()
+  }
 }
