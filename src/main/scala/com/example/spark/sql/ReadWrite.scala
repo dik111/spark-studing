5e109749dc18711d0565d11d421256c58d5d8c99
yuweiZhan <195812208@qq.com>
2020-09-29 22:09:13 +0800

    :sparkles: spark-sql writer

--- src/main/scala/com/example/spark/sql/ReadWrite.scala
+++ src/main/scala/com/example/spark/sql/ReadWrite.scala
@@ -43,4 +43,27 @@ class ReadWrite {
       .show(10)
   }
 
+  @Test
+  def writer1(): Unit ={
+
+    // 创建 sparkSession
+    val spark = SparkSession.builder()
+      .master("local[6]")
+      .appName("reader1")
+      .getOrCreate()
+
+    // 读取数据集
+    val df = spark.read
+      .option("header",value = true)
+      .option("inferSchema",value = true)
+      .csv("src/main/resources/BeijingPM20100101_20151231.csv")
+
+    // 写入数据集
+    df.write.json("src/main/resources/beijing_pm.json")
+
+    df.write.format("json").save("src/main/resources/beijing_pm2.json")
+
+
+  }
+
 }
